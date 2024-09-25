9c27a63b291f4695b1ef0fb61cbca2f3733458a7
Fernando Andres-Pretel <feranpre@gmail.com>
2024-09-25 20:58:12 +0200

    cosas

--- lua/hades/plugins/notes/render-markdown.lua
+++ lua/hades/plugins/notes/render-markdown.lua
@@ -2,6 +2,24 @@ return {
   "MeanderingProgrammer/render-markdown.nvim",
   opts = {
     file_types = { "markdown", "norg", "rmd", "org", "quarto" },
+    checkbox = {
+      enabled = true,
+      position = "inline",
+      unchecked = {
+        icon = " ",
+        highlight = "RenderMarkdownUnchecked",
+      },
+      checked = {
+        -- Replaces '[x]' of 'task_list_marker_checked'
+        icon = " ",
+        -- Highligh for the checked icon
+        highlight = "RenderMarkdownChecked",
+      },
+      custom = {
+        todo = { raw = "[.]", rendered = " " },
+        not_happening = { raw = "[-]", rendered = "󰬅 " },
+      },
+    },
   },
   dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
   ft = { "markdown", "norg", "rmd", "org", "quarto" },
