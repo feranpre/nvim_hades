ea5c6dc86f289bfad878f022fecb70b150318cd9
Fernando Andres-Pretel <feranpre@gmail.com>
2024-10-24 17:16:41 +0200

    asdf

--- lua/hades/config/colors.lua
+++ lua/hades/config/colors.lua
@@ -12,8 +12,14 @@ vim.cmd.colorscheme("dracula-soft")
 -- vim.cmd("colorscheme onedark")
 -- vim.cmd("colorscheme monokai-pro")
 
+-- Onedark config
+-- require("onedark").setup({
+--   style = "warm",
+-- })
+-- require("onedark").load()
+
 -- local colors = require 'catppuccin.palettes.mocha'
 -- vim.api.nvim_set_hl(0, 'Tabline', { fg = colors.green, bg = colors.mantle })
 -- vim.api.nvim_set_hl(0, 'TermCursor', { fg = '#A6E3A1', bg = '#A6E3A1' })
-vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "#383A46" }) -- numero de linea
-vim.api.nvim_set_hl(0, "CursorLine", { bg = "#383A46" }) -- numero de linea
+-- vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "#383A46" }) -- numero de linea
+-- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#383A46" }) -- numero de linea
