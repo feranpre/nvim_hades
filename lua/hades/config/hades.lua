6946224de49452dfcc9584a4e22c327f3d670f49
Fernando Andres-Pretel <feranpre@gmail.com>
2025-01-31 20:20:35 +0100

    oldworld

--- lua/hades/config/hades.lua
+++ lua/hades/config/hades.lua
@@ -12,12 +12,13 @@ return {
   -- require("tokyonight").load()
   -- end,
   -- colorscheme = "tokyonight-storm",
-  colorscheme = "catppuccin-frappe",
+  -- colorscheme = "catppuccin-frappe",
+  colorscheme = "oldworld",
 
   -- load the default settings
   defaults = {
     autocmds = true, -- lazyvim.config.autocmds
-    keymaps = true,  -- lazyvim.config.keymaps
+    keymaps = true, -- lazyvim.config.keymaps
     -- lazyvim.config.options can't be configured here since that's loaded before lazyvim setup
     -- if you want to disable loading options, add `package.loaded["lazyvim.config.options"] = true` to the top of your init.lua
   },
