fc4da345d1b8b219b528a0b8d76dd0c30dcaa8f8
Fernando Andres-Pretel <feranpre@gmail.com>
2025-03-17 20:20:37 +0100

    blink

--- lua/hades/plugins/lsp/blink.lua
+++ lua/hades/plugins/lsp/blink.lua
@@ -1,6 +1,7 @@
 return {
   {
     "saghen/blink.cmp",
+    dependencies = { "ribru17/blink-cmp-spell" },
     version = "*",
     -- dependencies = "rafamadriz/friendly-snippets",
     -- dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
@@ -16,7 +17,7 @@ return {
       },
 
       sources = {
-        default = { "cmp_r", "lazydev", "lsp", "path", "snippets", "buffer" },
+        default = { "snippets", "cmp_r", "lazydev", "lsp", "spell", "path", "buffer" },
         providers = {
           lsp = {
             name = "LSP",
@@ -32,6 +33,27 @@ return {
             module = "blink.compat.source",
             opts = {
               filetypes = { "r", "rmd", "quarto" },
+            },
+          },
+          spell = {
+            name = "Spell",
+            module = "blink-cmp-spell",
+            opts = {
+              -- EXAMPLE: Only enable source in `@spell` captures, and disable it
+              -- in `@nospell` captures.
+              enable_in_context = function()
+                local curpos = vim.api.nvim_win_get_cursor(0)
+                local captures = vim.treesitter.get_captures_at_pos(0, curpos[1] - 1, curpos[2] - 1)
+                local in_spell_capture = false
+                for _, cap in ipairs(captures) do
+                  if cap.capture == "spell" then
+                    in_spell_capture = true
+                  elseif cap.capture == "nospell" then
+                    return false
+                  end
+                end
+                return in_spell_capture
+              end,
             },
           },
         },
@@ -52,6 +74,20 @@ return {
         -- Displays a preview of the selected item on the current line
         ghost_text = {
           enabled = false,
+        },
+      },
+      fuzzy = {
+        sorts = {
+          function(a, b)
+            local sort = require("blink.cmp.fuzzy.sort")
+            if a.source_id == "spell" and b.source_id == "spell" then
+              return sort.label(a, b)
+            end
+          end,
+          -- This is the normal default order, which we fall back to
+          "score",
+          "kind",
+          "label",
         },
       },
     }, -- opts
