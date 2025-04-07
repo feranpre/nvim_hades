51a4ab8ad3912aa704b71426a32b7749f9bb0b91
Fernando Andres-Pretel <feranpre@gmail.com>
2025-04-07 12:00:06 +0200

    retocando lsp

--- lua/hades/plugins/code/mini-surround.lua
+++ lua/hades/plugins/code/mini-surround.lua
@@ -1,6 +1,7 @@
 return {
-  { 'echasnovski/mini.surround', 
-  version = '*',
+  { "echasnovski/mini.surround", 
+  version = "*",
+    enabled = true,
   config = function ()
     require("mini.surround").setup()
   end
