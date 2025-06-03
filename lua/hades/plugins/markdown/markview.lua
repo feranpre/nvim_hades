36010227fa2e9d04438458ec6f3cbaa47b77f911
Fernando Andres-Pretel <feranpre@gmail.com>
2025-06-03 13:28:02 +0200

    asdf

--- lua/hades/plugins/markdown/markview.lua
+++ lua/hades/plugins/markdown/markview.lua
@@ -1,8 +1,8 @@
 return {
   "OXY2DEV/markview.nvim",
-  enabled = false,
+  enabled = true,
   lazy = false, -- Recommended
-  branch = "dev",
+  -- branch = "dev",
   -- priority = 500,
   -- ft = "markdown" -- If you decide to lazy-load
   dependencies = {
