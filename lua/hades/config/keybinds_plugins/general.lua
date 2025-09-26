aceba95fb6931b107975785ee84f68f3b38eed06
Fernando Andres-Pretel <feranpre@gmail.com>
2025-09-26 10:11:43 +0200

    asdf

--- lua/hades/config/keybinds_plugins/general.lua
+++ lua/hades/config/keybinds_plugins/general.lua
@@ -1,5 +1,5 @@
 local keymap = require("hades.utils.keys").keymap
-local keymap_buffer = require("hades.utils.keys").keymap_buffer
+-- local keymap_buffer = require("hades.utils.keys").keymap_buffer
 local key_or_clue = require("hades.utils.keys").key_or_clue
 
 local opts = { noremap = true, silent = true, desc = nil }
