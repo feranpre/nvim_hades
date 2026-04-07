33e0e6f790f29781af27f103f7de7b2f191439b0
Fernando Andres-Pretel <feranpre@gmail.com>
2026-04-07 16:22:10 +0200

    asdf

--- lua/hades/plugins/editor/flash.lua
+++ lua/hades/plugins/editor/flash.lua
@@ -1,13 +1,34 @@
 return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  ---@type Flash.Config
-  opts = {},
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "[s]earch (flash)" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "[S]elect treesitter (flash)" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "[r]emote (flash)" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Search treesitter (flash)" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	---@type Flash.Config
+	opts = {},
+	keys = {
+		{
+			"S",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "[s]earch (flash)",
+		},
+		-- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "[S]elect treesitter (flash)" },
+		{
+			"R",
+			mode = "o",
+			function()
+				require("flash").remote()
+			end,
+			desc = "[r]emote (flash)",
+		},
+		-- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Search treesitter (flash)" },
+		{
+			"<c-s>",
+			mode = { "c" },
+			function()
+				require("flash").toggle()
+			end,
+			desc = "Toggle Flash Search",
+		},
+	},
 }
